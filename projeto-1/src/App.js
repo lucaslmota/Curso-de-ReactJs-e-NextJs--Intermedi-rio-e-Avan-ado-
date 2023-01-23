a035e3d9b9349cde730da57a3935932b6dd47aa8
lucaslmota <lucaslmota0431@gmail.com>
2023-01-23 14:41:18 -0300

    first commit

---
+++ projeto-1/src/App.js
@@ -0,0 +1,43 @@
+import { Component } from 'react';
+import './App.css';
+
+class App extends Component{
+  state = {
+    posts:[
+      {
+        id: 1,
+        title: 'Capitulo 1',
+        body: 'Corpo 1'
+      },
+      {
+        id: 2,
+        title: 'Capitulo 2',
+        body: 'Corpo 2'
+      },
+      {
+        id: 3,
+        title: 'Capitulo 3',
+        body: 'Corpo 3'
+      }
+    ]
+  }
+
+  render(){
+    const {posts} = this.state;
+    return(
+      <div className='App'>
+          {
+            posts.map(post => (
+              <div key={post.id}>
+                <h1 key={post.id}>{post.title}</h1>
+                <p>{post.body}</p>
+              </div>
+            ))
+          }
+      </div>
+    )
+  }
+
+}
+
+export default App;
